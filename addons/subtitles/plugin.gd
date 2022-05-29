e7990341b8c9d4af9bece08db9dd02fea53e384c
Bryce "BtheDestroyer" Dixon <pokemonbrycedixon@gmail.com>
2022-05-29 16:37:39 -0700

    Fixed usage tracking

--- addons/subtitles/plugin.gd
+++ addons/subtitles/plugin.gd
@@ -7,7 +7,7 @@ func _enter_tree():
 	var http = HTTPRequest.new()
 	add_child(http)
 	var project_hash = ProjectSettings.get_setting("application/config/name").sha256_text()
-	http.request("https://pluginstats.brycedixon.dev/", [], true, HTTPClient.METHOD_POST, JSON.print({plugin="ArgParse", project=project_hash}))
+	http.request("https://pluginstats.brycedixon.dev/", [], true, HTTPClient.METHOD_POST, JSON.print({plugin="Subtitles", project=project_hash}))
 	# Usage Tracking
   add_autoload_singleton(SubtitleManager.NAME, "res://addons/subtitles/SubtitleManager.gd")
   add_custom_type("SubtitleInterface", "Node", preload("res://addons/subtitles/SubtitleInterface.gd"), preload("res://addons/subtitles/SubtitleInterface.svg"))
