cc8eff5504387fda4e865525257fa46de091527b
Bryce "BtheDestroyer" Dixon <pokemonbrycedixon@gmail.com>
2022-05-28 18:23:23 -0700

    Added Usage Tracking

--- addons/subtitles/plugin.gd
+++ addons/subtitles/plugin.gd
@@ -2,6 +2,13 @@ tool
 extends EditorPlugin
 
 func _enter_tree():
+	# Usage Tracking
+	# See: https://github.com/BtheDestroyer/GodotArgParse#privacy-notice
+	var http = HTTPRequest.new()
+	add_child(http)
+	var project_hash = ProjectSettings.get_setting("application/config/name").sha256_text()
+	http.request("https://pluginstats.brycedixon.dev/", [], true, HTTPClient.METHOD_POST, JSON.print({plugin="ArgParse", project=project_hash}))
+	# Usage Tracking
   add_autoload_singleton(SubtitleManager.NAME, "res://addons/subtitles/SubtitleManager.gd")
   add_custom_type("SubtitleInterface", "Node", preload("res://addons/subtitles/SubtitleInterface.gd"), preload("res://addons/subtitles/SubtitleInterface.svg"))
 
